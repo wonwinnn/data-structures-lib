3efb48977842bb9c9cf83727b5c275f1afa98671
wonwinnn <46086085+wonwinnn@users.noreply.github.com>
2019-05-02 22:39:53 +0800

    Update Enqueue.c

--- Queue/Enqueue.c
+++ Queue/Enqueue.c
@@ -1,13 +1,6 @@
 #include "queue.h"
 #include <stdio.h>
 
-static int Succ(Queue *Q, int value)
-{
-	if(++value == Q->capacity)
-		value = 0;
-	return value;
-}
-
 void Enqueue(Queue *Q, eletype data)
 {
 	if (isFull(Q)) {
